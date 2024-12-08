7c67cd31a5919581221e57eddab606a3d0f0487d
Alexandria Cox <alexacox.atc@gmail.com>
2024-12-07 21:09:19 -0500

    style: Cleaned up code for footer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'; // import entire contents of a file
 import Home from './pages/Home'; // importing from a local file
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 import { Container } from 'reactstrap';
 
 /*
@@ -11,10 +12,12 @@ import { Container } from 'reactstrap';
 */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
+  <div className='min-vh-100'>
+    {/*I am trying to get the footer to push to the bottom with bootstrap making the min height of the element 100vh but its not working. */}
     <Navigation />
-    <Container className="mt-3">
+    <Container className='mt-3'>
       <Home />
     </Container>
+    <Footer />
   </div>
 );
