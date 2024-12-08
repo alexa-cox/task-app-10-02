5763bc05342f3fa7c7c99d91fa9d5a4f50926bef
Alexandria Cox <alexacox.atc@gmail.com>
2024-12-07 21:09:19 -0500

    style: Cleaned up code for footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer class='py-3 my-4'>
+      <p className='text-center text-body-secondary'>© 2024 Company, Inc</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Card, CardHeader, CardBody, CardFooter, Button } from 'reactstrap';
 import TodoList from '../components/TodoList';
 
 /*
