519f92fc17eeb4f60f8069d799c75d21de1b6e41
Nas Baki <nas.baki@holman.com>
2024-12-14 12:03:38 -0500

    Week 2

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import { Button, Card, CardHeader, CardBody, CardFooter } from 'reactstrap';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Button, Card, CardHeader, CardBody, CardFooter, Input } from 'reactstrap';
 
+/*
+  React State Management:
+
+  - Special mechanism to communicate to React Framework when we need our component
+    to re-render with updates
+  
+    Hooks:
+      - useState(): Update the state of a React Component
+        - Returns an array of two elements
+          - 1st: Pointer to the data you're tracking
+          - 2nd: A function to update the data
+      
+          Example:
+          let results = useState();
+          let addedItem = results[0];
+          let setAddedItem = results[1];
+*/
+
+// <TodoList items={[]} /> = TodoList();
 function TodoList(data) {
-  // data.items = array
+  const [ addedItem, setAddedItem ] = useState(false);
+  const [ items, setItems ] = useState(data.items);
+  const [ newItem, setNewItem ] = useState();
+  
+  const addNewItem = () => {
+    setAddedItem(true); // provide updated value as argument
+    console.log('Changing addItem to true', addedItem);
+    
+  }
 
   const showButton = () => {
     if (data.showNewButton) {
       return (
         <CardFooter>
           <Button
+            onClick={addNewItem}
             color='success'
             size='sm'
           >
@@ -29,8 +58,8 @@ function TodoList(data) {
       isChecked = true;
       checkClass = 'bg-secondary border-secondary';
     }
-
-    const jsxItems = data.items.map(function (item) {
+    
+    const jsxItems = items.map(function (item) {
       return (
         <li className={`${completedClass} list-group-item`}>
           <input
@@ -38,7 +67,7 @@ function TodoList(data) {
             type='checkbox'
             checked={isChecked}
           />
-          <span className='ms-3'>{item}</span>
+          <Link to={`/todo/${item}`}><span className='ms-3'>{item}</span></Link>
         </li>
       );
     });
@@ -46,12 +75,34 @@ function TodoList(data) {
     return jsxItems;
   };
 
+  const showAddedItemInput = () => {
+    if (addedItem) {
+      return (
+        <Row>
+          <Col><Input onChange={(event) => setNewItem(event.target.value)} /></Col>
+          <Col><Button onClick={saveItem} color="primary">Save</Button></Col>
+        </Row>
+      )
+    }
+  }
+
+  /*
+    Immutablity: Not being to mutate the original value of something
+  */
+  const saveItem = () => {
+    let newItems = items.concat([newItem]); // Ensure we create a brand new array
+
+    // Update the state holding the items array
+    setItems(newItems);
+  }
+
   return (
     <section className='todo-list mt-2'>
       <Card>
         <CardHeader>{data.title}</CardHeader>
         <CardBody>
           <ul className='list-group list-group-flush'>{showItems()}</ul>
+          {showAddedItemInput()}
         </CardBody>
 
         {showButton()}
@@ -61,3 +112,14 @@ function TodoList(data) {
 }
 
 export default TodoList;
+
+/*
+function sayName() {
+  console.log('My name is Jolie');
+};
+
+sayName(); // My name is Nas
+sayName(); // My name is Mary
+sayName(); // My name is Bob
+sayName(); // My name is Jolie
+*/
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import Home from './pages/Home'; // importing from a local file
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import { Container } from 'reactstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TodoList from './components/TodoList';
+import TodoItem from './components/TodoItem';
 
 /*
   Preparing the React Tree into your HTML document
@@ -13,11 +16,33 @@ import { Container } from 'reactstrap';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className='min-vh-100'>
-    {/*I am trying to get the footer to push to the bottom with bootstrap making the min height of the element 100vh but its not working. */}
-    <Navigation />
-    <Container className='mt-3'>
-      <Home />
-    </Container>
-    <Footer />
+    <BrowserRouter>
+      {/*I am trying to get the footer to push to the bottom with bootstrap making the min height of the element 100vh but its not working. */}
+      <Navigation />
+
+
+      <Container className='mt-3'>
+        {/* Create Routes (pages) */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/active-items' element={<TodoList
+            title='My Active List'
+            showNewButton={true}
+            completed={false}
+            items={['Do Laundry', 'Clean Room']}
+          />} />
+          <Route path='/completed-items' element={<TodoList
+            title='Completed List'
+            showNewButton={false}
+            completed={true}
+            items={['Take Out Trash', 'Make Dinner']}
+          />} />
+
+          <Route path="/todo/:itemName" element={<TodoItem />} />
+
+        </Routes>
+        <Footer />
+      </Container>
+    </BrowserRouter>
   </div>
 );
